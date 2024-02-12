6a07526d42ed46a66db01551e2f8db794f5d6026
SIRA-RPIRobotics <SIRA.rpi.robotics@gmail.com>
2024-02-12 12:04:44 -0500

    added catkin_ws

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sirab/SIRA-Formation-Control/catkin_ws/src
+# Build directory: /home/sirab/SIRA-Formation-Control/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
