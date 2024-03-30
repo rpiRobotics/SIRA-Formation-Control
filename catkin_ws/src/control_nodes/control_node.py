471d8ae4d9d8871c0e83ddd823a2e5e6db7a8327
SIRA-RPIRobotics <SIRA.rpi.robotics@gmail.com>
2024-03-30 16:51:38 -0400

    worked ongetting ros readouts working correctly

--- catkin_ws/src/control_nodes/control_node.py
+++ catkin_ws/src/control_nodes/control_node.py
@@ -5,6 +5,9 @@ import geometry_msgs.msg # for Twist
 import std_msgs.msg # for JointState
 import numpy as np
 import socket
+import tf2_ros
+import yaml
+import time
 from scipy.spatial.transform import Rotation as scp
 from scipy.spatial.transform import Slerp
 
@@ -18,42 +21,51 @@ class followerPosControl():
         self.ld = 5
         self.psid = np.pi
 
+        self.tfBuffer = tf2_ros.Buffer()
+        self.tfListener = tf2_ros.TransformListener(self.tfBuffer)
+
         #Subscribed messages
         self.name = socket.gethostname()
         if self.name == 'sirab-T15':
             self.name = 'sira_b'
             self.leader = 'sira_r'
+            self.leader_frame = 'ridgeRframe_from_marker_left'
+            self.follower_frame = 'ridgeBframe'
         else:
             self.name = 'sira_r'
             self.leader = 'sira_b'
+            self.leader_frame = 'ridgeBframe_from_marker_right'
+            self.follower_frame = 'ridgeRframe'
 
-        self.leader_position = rospy.Subscriber('/base/'+self.leader+'_rel_pos',
-                                                geometry_msgs.msg.PoseStamped,
-                                                self.leader_pos_update_callback,
-                                                queue_size=1)
-        self.leader_vel = rospy.Subscriber()
+        #self.leader_vel = rospy.Subscriber('/' + self.leader + '/ridgeback/vel_feedback',std_msgs.msg.String,
+                                                 #self.taskmodecallback,
+                                                 #queue_size=1)
         #Published messages
-        self.sira_vel = rospy.Publisher('/'+self.name+'/sira_follower_vel', geometry_msgs.msg.Twist,queue_size=1)
+        #self.sira_vel = rospy.Publisher('/'+self.name+'/sira_follower_vel', geometry_msgs.msg.Twist,queue_size=1)
+        frames_dict = yaml.safe_load(self.tfBuffer.all_frames_as_yaml())
+        frames_list = list(frames_dict.keys())
 
+        print(self.calcRelPosition())
 
-    def leader_pos_update_callback(self,new_leader_pos):
+    def calcRelPosition(self):
+        time.sleep(10)
+        self.T_human2ee = self.tfBuffer.lookup_transform(self.leader_frame , self.follower_frame,rospy.Time())
+        return self.T_human2ee
+
+    def leaderPosUpdateCallback(self,new_leader_pos):
         self.leader_pos[0] = new_leader_pos.pose.position.x
         self.leader_pos[1] = new_leader_pos.pose.position.y
         self.leader_pos[2] = new_leader_pos.pose.position.z
 
-    def calc_angle(self,x,y,ang_in):
+    def calcAngle(self,x,y,ang_in):
         angle = np.atan2(x,y)
         if abs(angle-ang_in) > 5:
             if angle > ang_in:
                 self.theta = angle-2*np.pi
             else:
                 self.theta = angle+2*np.pi
-    def calc_rel_pos(self):
-        x = self.leader_pos[0] - self.cur_pos[0]
-        y = self.leader_pos[1] - self.cur_pos[1]
-        self.l = np.sqrt(x**2 + y**2)
-        self.theta = self.calc_angle(x,y,self.theta)
-    def calc_vel(self):
+
+    def calcVel(self):
         error = np.array([[self.k1*(self.ld-self.l)],[self.k2*(self.thetad-self.theta)]])
         uj = error - self.leader_vel
         self.sira_vel = np.dot(np.array([[np.cos(self.theta), 0],[np.sin(self.theta),0],[0, 1]]), uj)
