bef2fc5df32eeca22a23300f431811cdb3f455de
Joel McCandless <mail@joelmccandless.com>
2024-04-24 21:46:59 -0400

    stop checking environment variable. hostname is enough.

--- catkin_ws/src/control_nodes/identity.py
+++ catkin_ws/src/control_nodes/identity.py
@@ -1,12 +1,11 @@
 import socket
-import os
 
 sira_name = socket.gethostname()
 sira_follower = 'sirar'
 sira_leader = 'sirab'
 sira_leader_frame = 'ridgeBframe_from_marker_right'
 sira_follower_frame = 'ridgeRframe'
-if sira_name == 'sirab-T15' or os.environ['SIRAB_OR_SIRAR'] == 'sirab':
+if sira_name == 'sirab-T15':
     sira_follower = 'sirab'
     sira_leader = 'sirar'
     sira_leader_frame = 'ridgeRframe_from_marker_left'
