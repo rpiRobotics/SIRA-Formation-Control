9cd484f0acd4ce9de0fd406058cbe1f7523a28f8
Joel McCandless <mail@joelmccandless.com>
2024-04-24 21:44:10 -0400

    check environment variable SIRAB_OR_SIRAR for identifying

--- catkin_ws/src/control_nodes/identity.py
+++ catkin_ws/src/control_nodes/identity.py
@@ -1,11 +1,12 @@
 import socket
+import os
 
 sira_name = socket.gethostname()
 sira_follower = 'sirar'
 sira_leader = 'sirab'
 sira_leader_frame = 'ridgeBframe_from_marker_right'
 sira_follower_frame = 'ridgeRframe'
-if sira_name == 'sirab-T15':
+if sira_name == 'sirab-T15' or os.environ['SIRAB_OR_SIRAR'] == 'sirab':
     sira_follower = 'sirab'
     sira_leader = 'sirar'
     sira_leader_frame = 'ridgeRframe_from_marker_left'
