754388dc36c715770d606d13875528e777011fb5
Fan <fdu7777@gmail.com>
2025-01-26 15:58:03 -0800

    fix for nan in reading tensor
    changed np.delete treatment to assume old numpy version
    refactored uwb_delay DelayEstimator methods

--- catkin_ws/src/sensors/uwb_delay.py
+++ catkin_ws/src/sensors/uwb_delay.py
@@ -3,6 +3,9 @@
 # This node is for estimating uwb delays using onboard data
 # and data visualization
 
+# TODO(fan.du): check deletion is working correctly
+#   might be due to numpy version not treating binary as mask, check if argwhere fixes
+
 from uwb_data import *
 import sys
 import matplotlib.pyplot as plt
@@ -18,6 +21,7 @@ delays = {
     '48C4': 0,
     'C326': 0,
 }
+
 class DelayEstimator:
     def __init__(self):
         rospy.init_node('delay_test', anonymous=False)
@@ -34,71 +38,73 @@ class DelayEstimator:
         self.read_indices = np.array([0,0,0,0])
 
         # temporary hack since using 3 tags only
+        # TODO(fan.du): remove this section once using 4 tags
         self.readings[:,-1,:] = np.zeros([self.num_readings,4])
         self.read_indices[-1] = self.num_readings
 
+    def check_full(self):
+        return np.sum(self.read_indices) >= 4*self.num_readings
+
     def uwb_callback(self, data):
-        # data = data.data.strip().split(',')
             
         # if np.isnan(np.sum(self.readings)):
-
             # NOTE: this is a temporary workaround for the following issue:
             # sometimes tags only get 3 readings instead of 4 and thus a nan is leftover
             # a proper fix would involve tracking the anchor indices but currently we delete
             # the nan rows
         if np.sum(self.read_indices) < 4*self.num_readings:
-
             tag, anchors, dists = parse_reading(data.data)
             if self.read_indices[tag_indices[tag]] < self.num_readings:
                 for i in range(len(anchors)):
                     self.readings[self.read_indices[tag_indices[tag]],tag_indices[tag],anchor_indices[anchors[i]]] = dists[i]
                 self.read_indices[tag_indices[tag]] += 1
+
+    def calc_delays(self):
+        # NOTE: see uwb_callback note
+        if np.isnan(np.sum(self.readings)):
+            print('nans present, eliminating nan rows')
+            self.readings = np.delete(self.readings, np.argwhere(np.any(np.isnan(self.readings), axis=(1,2))),axis=0)
+        print(self.readings.shape)
+        assert(not np.isnan(np.sum(self.readings)))
+
+        avg = np.mean(self.readings,axis=0)
+        # estimate delays here
+        # for tag and anchors t1, t2, a1, a2
+        # we need readings t1a1, t1a2, t2a1, t2a2
+        t1 = '0D40'
+        t2 = '0418'
+        a1 = '43E6'
+        a2 = '2A32'
         
-        else:
-
-            # NOTE: see previous note
-            if np.isnan(np.sum(self.readings)):
-                print('nans present, eliminating nan rows')
-                self.readings = np.delete(self.readings, np.all(np.isnan(self.readings), axis=(1,2)),axis=0)
-            print(self.readings.shape)
-            assert(not np.isnan(np.sum(self.readings)))
-
-            avg = np.mean(self.readings,axis=0)
-            # estimate delays here
-            # for tag and anchors t1, t2, a1, a2
-            # we need readings t1a1, t1a2, t2a1, t2a2
-            t1 = '0D40'
-            t2 = '0418'
-            a1 = '43E6'
-            a2 = '2A32'
+        r11 = avg[tag_indices[t1],anchor_indices[a1]]
+        r12 = avg[tag_indices[t1],anchor_indices[a2]]
+        r21 = avg[tag_indices[t2],anchor_indices[a1]]
+        r22 = avg[tag_indices[t2],anchor_indices[a2]]
+        
+        gt11 = 1.245
+        gt12 = 1.25
+        gt21 = 1.31
+        gt22 = 1.26
+        
+        # need to solve system r_vec = gt_vec + [1 0 1 0;1 0 0 1;0 1 1 0;0 1 0 1] * delay_vec
+        # = r_vec - gt_vec (b) = [1 0 1 0;1 0 0 1;0 1 1 0;0 1 0 1] (a) * delay_vec (x)
+        a = np.array([[1,0,1,0],[1,0,0,1],[0,1,1,0],[0,1,0,1]])
+        b = np.array([r11,r12,r21,r22])
+        b = b - np.array([gt11,gt12,gt21,gt22])
+        delays = np.linalg.solve(a,b)
+        print(delays)
+        new_est = np.array([r11,r12,r21,r22]) + a * delays
+        print(new_est)
+        fig, axs = plt.subplots(4,4)
+        for i in range(4):
+            for j in range(4):
+                count, bins = np.histogram(self.readings[:,i,j])
+                axs[i,j].hist(count,bins)
+        plt.show()
             
-            r11 = avg[tag_indices[t1],anchor_indices[a1]]
-            r12 = avg[tag_indices[t1],anchor_indices[a2]]
-            r21 = avg[tag_indices[t2],anchor_indices[a1]]
-            r22 = avg[tag_indices[t2],anchor_indices[a2]]
-            
-            gt11 = 1.245
-            gt12 = 1.25
-            gt21 = 1.31
-            gt22 = 1.26
-            
-            # need to solve system r_vec = gt_vec + [1 0 1 0;1 0 0 1;0 1 1 0;0 1 0 1] * delay_vec
-            # = r_vec - gt_vec (b) = [1 0 1 0;1 0 0 1;0 1 1 0;0 1 0 1] (a) * delay_vec (x)
-            a = np.array([[1,0,1,0],[1,0,0,1],[0,1,1,0],[0,1,0,1]])
-            b = np.array([r11,r12,r21,r22])
-            b = b - np.array([gt11,gt12,gt21,gt22])
-            delays = np.linalg.solve(a,b)
-            print(delays)
-            new_est = np.array([r11,r12,r21,r22]) + a * delays
-            print(new_est)
-            fig, axs = plt.subplots(4,4)
-            for i in range(4):
-                for j in range(4):
-                    count, bins = np.histogram(self.readings[:,i,j])
-                    axs[i,j].hist(count,bins)
-            plt.show()
-            sys.exit()
 
 if __name__ == '__main__':
     delays = DelayEstimator()
-    rospy.spin()
+    while not delays.check_full:
+        pass
+    delays.calc_delays()
